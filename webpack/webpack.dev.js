4de35e3e3cbcd0c4f3e72424b7fb2348948930e8
Artem Demo <artemdemo@gmail.com>
2018-10-20 14:14:18 +0300

    installed and configured babel + webpack

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,24 @@
+const webpackCommonFactory = require('./webpack.common');
+
+/**
+ * @param options {Object} - see required params in `webpackCommon.js`
+ */
+module.exports = (options) => {
+    const webpackCommon = webpackCommonFactory(options);
+    return Object.assign(webpackCommon, {
+        mode: 'development',
+        devtool: 'source-map',
+        optimization: {
+            minimize: false,
+        },
+        devServer: {
+            host: '0.0.0.0',
+            port: 8080,
+            contentBase: `${options.buildFolder}/`,
+            historyApiFallback: true,
+        },
+        plugins: [
+            ...webpackCommon.plugins,
+        ],
+    });
+};
