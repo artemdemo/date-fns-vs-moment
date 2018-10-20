66621af97bf8b609aca2829380f69519a3805904
Artem Demo <artemdemo@gmail.com>
2018-10-20 15:20:07 +0300

    utc formatting in date-fns is not supported

--- source/examples/dateFns.js
+++ source/examples/dateFns.js
@@ -2,11 +2,10 @@ import { format, differenceInDays, distanceInWords } from 'date-fns';
 
 const dateFnsExample = ($el) => {
     const timestamp = 1540035262000;
-    const utcString = new Date(1540035262000).toUTCString();
     $el.html(`
         <div>
             <p>Regular: ${format(timestamp, 'HH:mm:ss, YYYY-MM-DD')}</p>
-            <p>UTC: ${format(utcString, 'HH:mm:ss, YYYY-MM-DD')}</p>
+            <p>UTC: <strong>not supported</strong></p>
             <p>Duration in days: ${differenceInDays(1200000000, 0)} (provides difference in <strong>full</strong> days)</p>
             <p>Duration human readable: ${distanceInWords(1200000000, 0)}</p>
             <p>Timestamp: ${format(new Date(), 'x')}</p>
