3ba1ce647622b1cbd78c9a850d44b9b93d52cc82
Artem Demo <artemdemo@gmail.com>
2018-10-25 22:45:28 +0300

    added comment

--- source/index.js
+++ source/index.js
@@ -1,5 +1,8 @@
 import u from 'umbrellajs';
 
+// I'm using dynamic import in order to separate the code,
+// as well as get in one build both examples
+
 import('./examples/moment').then((e) => {
     e.default(u('.moment .examples'));
 });
